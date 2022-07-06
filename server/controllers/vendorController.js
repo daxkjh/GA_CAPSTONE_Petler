8df01041439b8d19667b33f40a8a3b79ebfb180b
avianna-wynks <103884884+avianna-wynks@users.noreply.github.com>
2022-07-06 14:41:15 +0800

    set up nextjs

--- server/controllers/vendorController.js
+++ server/controllers/vendorController.js
@@ -1,20 +1,23 @@
 const express = require('express')
 const router = express.Router();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
-router.get("/", (req, res) => {
-    res.send("checking")
+router.get("/", async (req, res) => {
+    const vendors = await prisma.vendor.findMany()
+    res.send(vendors)
 })
 
 router.post("/", (req, res) => {
-    res.send("post check!!!")
+    res.send("post check!!!!")
 })
 
 router.put("/:id", (req, res) => {
-    res.send("update check")
+    res.send("update check!!")
 })
 
 router.delete("/:id", (req, res) => {
-    res.send("Yo! delete!")
+    res.send("Yo! delete!!")
 })
 
 

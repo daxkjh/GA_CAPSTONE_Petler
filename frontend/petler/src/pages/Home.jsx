ce073eeda3c12ee0dde550cb61f5ca945a02a510
avianna-wynks <103884884+avianna-wynks@users.noreply.github.com>
2022-07-07 14:39:11 +0800

    update

--- frontend/petler/src/pages/Home.jsx
+++ frontend/petler/src/pages/Home.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { vendorAtom } from '../App';
 
 function Home() {
-  console.log(vendorAtom)
+  console.log("home",vendorAtom)
   return (
     <div>
       This is home
-      {vendorAtom ? <p>Hi!</p> : null}
+      {/* {vendorAtom ? <p>Hi!</p> : null} */}
     </div>
   )
 }
--- frontend/petler/src/pages/vendor_pages/VendorLogin.jsx
+++ frontend/petler/src/pages/vendor_pages/VendorLogin.jsx
@@ -15,7 +15,8 @@ const navigate = useNavigate();
     event.preventDefault();
     const loginData = {
     email: event.target.elements.email.value,
-    password: event.target.elements.password.value} 
+    password: event.target.elements.password.value } 
+
     axios.post( "/api/vendors/login", loginData)
       .then((response)=> 
       {const token = response.data.accessToken;

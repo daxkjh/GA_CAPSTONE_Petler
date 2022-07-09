a44036c298d0d9ba774beabd09ff19732f530221
daxkongjh <dax.k@live.com>
2022-07-10 02:55:14 +0800

    updated schema

--- server/controllers/vendorController.js
+++ server/controllers/vendorController.js
@@ -3,10 +3,10 @@ const router = express.Router();
 // const { PrismaClient } = require("@prisma/client");
 // const prisma = new PrismaClient();
 const prisma = require("../server");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { PrismaClient } = require("@prisma/client");
-const saltRounds = 10
+const saltRounds = 10;
 
 //get for test
 router.get("/", async (req, res) => {
@@ -16,60 +16,74 @@ router.get("/", async (req, res) => {
 
 //sign up
 router.post("/signup", async (req, res) => {
-  const vendorData = { email: req.body.email, 
-    password: await bcrypt.hash(req.body.password, saltRounds) }
-    console.log(vendorData)
+  const vendorData = {
+    email: req.body.email,
+    password: await bcrypt.hash(req.body.password, saltRounds),
+  };
+  console.log(vendorData);
   try {
     const vendor = await prisma.vendor.create({ data: vendorData });
-    const vendorProfile = await prisma.profile.create({ data: {vendorId: vendor.id} });
-    res.status(200).json({ status: "success", data: vendor })
+    const vendorProfile = await prisma.profile.create({
+      data: { vendorId: vendor.id },
+    });
+    res.status(200).json({ status: "success", data: vendor });
   } catch (error) {
-    res.status(400).json({ status: "failed", data: error })
-  }  
-}); 
+    res.status(400).json({ status: "failed", data: error });
+  }
+});
 
 //Vendor PASSWORD Change
-router.put("/signup/:id", async (req,res)=>{
-  const {id} = req.params
-  const newPassword = await bcrypt.hash(req.body.password,saltRounds)
+router.put("/signup/:id", async (req, res) => {
+  const { id } = req.params;
+  const newPassword = await bcrypt.hash(req.body.password, saltRounds);
   try {
-   const vendor = await prisma.vendor.update({
-     where:{ id: id },
-    data:{ password: newPassword}})
-   res.status(200).json({message:"Password Updated"})
+    const vendor = await prisma.vendor.update({
+      where: { id: id },
+      data: { password: newPassword },
+    });
+    res.status(200).json({ message: "Password Updated" });
   } catch (error) {
-   res.status(400).json({status:"error", message: error})
+    res.status(400).json({ status: "error", message: error });
   }
- })
+});
 
 // login
 router.post("/login", async (req, res) => {
   try {
-    const { email, password } = req.body
-    const vendorLogin = await prisma.vendor.findUnique({ where: { email: email } });
+    const { email, password } = req.body;
+    const vendorLogin = await prisma.vendor.findUnique({
+      where: { email: email },
+    });
     // console.log(vendorLogin)
-     if (!vendorLogin) {
-      res.status(400).json({status: "failed", data: "Username not found"})
+    if (!vendorLogin) {
+      res.status(400).json({ status: "failed", data: "Username not found" });
     } else {
-      const match = await bcrypt.compare(password, vendorLogin.password)
+      const match = await bcrypt.compare(password, vendorLogin.password);
       if (match) {
-        const id = vendorLogin.id
-        const accessToken = jwt.sign({id, role:"vendor"}, 
-        process.env.ACCESS_TOKEN_SECRET, {expiresIn: "1h",})
-        res.status(200).send({ accessToken: accessToken, data: vendorLogin.id })
+        const id = vendorLogin.id;
+        const accessToken = jwt.sign(
+          { id, role: "vendor" },
+          process.env.ACCESS_TOKEN_SECRET,
+          { expiresIn: "1h" }
+        );
+        res
+          .status(200)
+          .send({ accessToken: accessToken, data: vendorLogin.id });
       } else {
-        res.status(400).json({ status: "failed", data: "Incorrect email or Password" });
+        res
+          .status(400)
+          .json({ status: "failed", data: "Incorrect email or Password" });
       }
     }
   } catch (error) {
-    res.status(400).json({status: "error"});
+    res.status(400).json({ status: "error" });
   }
- })
+});
 
 // Auth
 const isAuth = (req, res, next) => {
   const auth = req.headers.authorization;
-  console.log(auth)
+  console.log(auth);
   if (!auth) {
     res.status(401).send({ status: "error", msg: "No header" });
   }
@@ -90,38 +104,40 @@ const isAuth = (req, res, next) => {
 // vendor profile show all for testing
 router.get("/profile", async (req, res) => {
   try {
-    const allProfile = await prisma.profile.findMany()
-    res.send(allProfile)
+    const allProfile = await prisma.profile.findMany();
+    res.send(allProfile);
   } catch (error) {
-    res.send({status:401, error:error })
+    res.send({ status: 401, error: error });
   }
-})
+});
 
-// vendor profile show 
+// vendor profile show
 router.get("/profile/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const vendorProfile = await prisma.profile.findUnique({
       where: {
-          vendorId: id,
+        vendorId: id,
       },
       include: {
-        details: { 
+        details: {
           include: {
-          petSize: true,
-          area: true }},
+            petSize: true,
+            area: true,
+          },
+        },
         bookings: true,
         reviews: true,
         posts: true,
         services: true,
       },
-    })
-    res.status(200).json({status: "success", data: vendorProfile})
-    console.log(vendorProfile)
+    });
+    res.status(200).json({ status: "success", data: vendorProfile });
+    console.log(vendorProfile);
   } catch (error) {
-    res.send({status:401, error:error })
+    res.send({ status: 401, error: error });
   }
-})
+});
 
 // vendor profile update
 router.put("/profile/:id", async (req, res) => {
@@ -134,76 +150,90 @@ router.put("/profile/:id", async (req, res) => {
   let xl = "";
 
   if (req.body.xs === "on") {
-    xs = true
+    xs = true;
   } else {
-    xs = false
-  };
-  if (req.body.xs === "on") {
-    s = true
+    xs = false;
+  }
+  if (req.body.s === "on") {
+    s = true;
   } else {
-    s = false
-  }; 
-  if (req.body.xs === "on") {
-    m = true
+    s = false;
+  }
+  if (req.body.m === "on") {
+    m = true;
   } else {
-    m = false
-  }; 
-  if (req.body.xs === "on") {
-    l = true
+    m = false;
+  }
+  if (req.body.l === "on") {
+    l = true;
   } else {
-    l = false
-  };
-  if (req.body.xs === "on") {
-    xl = true
+    l = false;
+  }
+  if (req.body.xl === "on") {
+    xl = true;
   } else {
-    xl = false
-  };
-  const profile  = {
-    name: req.body.name,
-    address: req.body.address,
-    phone: req.body.phone,
-    intro: req.body.intro,          
-    type: req.body.type,
-    profilePic: req.body.profilePic,          
-    start: req.body.start,
-    end: req.body.end,
-    details: { 
-          update : {where: profileId: 20 }, {data: {
-            svcdsc: req.body.svcdsc,          
-            petType: req.body.petType,}
-          //   petSize: { create: {
-          //     xs: xs,
-          //     s: s,
-          //     m: m,
-          //     l: l,
-          //     xl: xl
-          // }},   
-          //   petSize: { create: {
-          //     xs: xs,
-          //     s: s,
-          //     m: m,
-          //     l: l,
-          //     xl: xl
-          // }}, 
-          // area: { update: {where:{id: 7} , data: {
-      //   north: true,
-      //   south: true,
-      //   east: true,
-      //   west: true
-      // }}}
-        }}}
-  console.log(profile)
-  try {
-    const vendorProflie = await prisma.profile.update({ 
-      where: { vendorId: id },
-      data: profile
-  })
-    res.status(200).json({status: "success", data: vendorProflie})
-  } catch (error) {
-     res.status(400).json({ status: "failed", data: error })
-  }  
-});
+    xl = false;
+  }
+  console.log(xs, s, m, l, xl);
+  console.log(req.body.xs, req.body.s, req.body.m);
 
+  try {
+    // const vendor = await prisma.vendor.findUnique({
+    //   where: { id: id },
+    //   include : { profile : { include: { details : {include: {petSize: true, area:true}}}},
+    //  }});
+    // console.log("VENDOR", vendor);
+
+    const vendorProfile = await prisma.profile.update({
+      where: { vendorId: id },
+      data: {
+        name: req.body.name,
+        address: req.body.address,
+        phone: req.body.phone,
+        intro: req.body.intro,
+        type: req.body.type,
+        profilePic: req.body.profilePic,
+        start: req.body.start,
+        end: req.body.end,
+        details: {
+          update: {
+            where: { profileId: vendor.profile.id },
+            data: {
+              svcdsc: req.body.svcdsc,
+              petType: req.body.petType,
+              petSize: {
+                update: {
+                  where: { detailsId: vendor.profile.details.id },
+                  data: {
+                    xs: xs,
+                    s: s,
+                    m: m,
+                    l: l,
+                    xl: xl,
+                  },
+                },
+              },
+              area: {
+                update: {
+                  where: { detailsId: 1 },
+                  data: {
+                    north: req.body.north,
+                    south: req.body.south,
+                    east: req.body.east,
+                    west: req.body.west,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+    res.status(200).json({ status: "success", data: vendor });
+  } catch (error) {
+    res.status(400).json({ status: "failed", data: error });
+  }
+});
 
 // vendor acount delete
 router.delete("/profile/:id", async (req, res) => {
@@ -212,16 +242,16 @@ router.delete("/profile/:id", async (req, res) => {
     const deleteProfile = await prisma.profile.delete({
       where: {
         vendorId: id,
-      }
-    })
+      },
+    });
     const deleteVendor = await prisma.vendor.delete({
       where: {
         id: id,
-      }
-    })
-    res.status(200).json({status: "user deleted ",data: deleteVendor});
+      },
+    });
+    res.status(200).json({ status: "user deleted ", data: deleteVendor });
   } catch (error) {
-    res.send({status: "failed ", data: "something went wrong"})
+    res.send({ status: "failed ", data: "something went wrong" });
   }
 });
 
@@ -229,7 +259,6 @@ router.delete("/profile/:id", async (req, res) => {
 router.get("/secret", isAuth, (req, res) => {
   res.send({ status: "ok", msg: "it's a secret" });
 });
-
 
 //   const data = req.body;
 //   const vendorLogin = await prisma.vendor.findOne({ data });
@@ -247,7 +276,5 @@ router.get("/secret", isAuth, (req, res) => {
 //     }
 //   }
 // });
-
-
 
 module.exports = router;
